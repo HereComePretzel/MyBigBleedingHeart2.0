48dddc371c5d16499ad14e7981e67c377c16f4f4
HereComePretzel <austinoie@gmail.com>
2021-02-04 15:08:36 -0600

    more boilerplate api axios

--- src/components/DashboardContainer.js
+++ src/components/DashboardContainer.js
@@ -3,26 +3,28 @@ import axios from 'axios'
 import Calendar from './Calendar'
 import AddPostModal from './AddPostModal'
 import HMModal from './HMModal'
-import PiechartModal from './PiechartModal'
-import GraphModal from './GraphModal'
+import Piechart from './Piechart'
+import Graph from './Graph'
 
 const DashboardContainer = () => {
     const [posts, setPosts] = useState([])
     
-    //upon initial rendering of component, all posts are pulled from the backend and stored w/ setPosts (can we find a way to filter these posts before they are pulled?) ie res.data.user_id === signed in user? try using axios params
+    //upon initial rendering of component, all posts are pulled from the backend and stored w/ setPosts (can we find a way to filter these posts before they are pulled?) ie res.data.user_id === signed in user? try using axios param
+
     useEffect(() => {
         axios.get('http://localhost:3000/posts')
-        .then(res => setPosts(res))
+        .then(res => setPosts(res.data))
         .catch(err => console.error(err))
+        // console.log(posts)
     }, [])
 
     return (
         <div>
             <AddPostModal />
-            <Calendar posts={posts.data}/>
-            <HMModal posts={posts.data}/>
-            <PiechartModal posts={posts.data}/>
-            <GraphModal posts={posts.data}/>
+            <Calendar posts={posts}/>
+            <HMModal posts={posts}/>
+            <Piechart posts={posts}/>
+            <Graph posts={posts}/>
         </div>
     )
 }
